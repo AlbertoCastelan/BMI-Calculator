2f5e4d42f1f3a79f789c00d38da9836ecfd154bc
AlbertoCastelan <albertovelacastelan@gmail.com>
2019-04-10 14:06:02 +0100

    Add files via upload

--- BMI Calculator/js/main.js
+++ BMI Calculator/js/main.js
@@ -2,7 +2,7 @@
 var age = document.getElementById('age').value;
 var male = document.getElementById('male').value;
 var female = document.getElementById('female').value;
-var index = document.querySelector('.bmiindex');
+var index = document.querySelector('.result');
 var button = document.getElementById('button');
 
 // Create an event listener
@@ -23,14 +23,30 @@ function bmiCalculator(e, weight, height) {
   result = Math.round(result);
   console.log(result);
   if (result <= 18.5) {
-    index.innerHTML = `<p>Your body mass index is ${result}, you are underweight.</p>`;
+    index.innerHTML = `<div class="show-result">
+    <p>Your body mass index is ${result}, you are underweight, try a diet rich on carbs and proteins.</p>
+    </div>`;
+    index.style.backgroundColor = '#48ACEE';
   } else if (result > 18.5 && result <= 24.9) {
-    index.innerHTML = `<p>Your body mass index is ${result}, you have an average weight.</p>`;
+    index.innerHTML = `<div class="show-result">
+    <p>Your body mass index is ${result}, you have an average weight, keep on the good work!.</p>
+    </div>`;
+    index.style.backgroundColor = '#27C122';
   } else if (result >= 25 && result <= 29.9) {
-    index.innerHTML = `<p>Your body mass index is ${result}, you are overweight.</p>`;
+    index.innerHTML = `<div class="show-result">
+    <p>Your body mass index is ${result}, you are overweight, take better care of your daily diet.</p>
+    </div>`;
+    index.style.backgroundColor = '#F98205';
   } else if (result >= 30 && result <= 34.9) {
-    index.innerHTML = `<p>Your body mass index is ${result}, you are obese.</p>`;
+    index.innerHTML = `<div class="show-result">
+    <p>Your body mass index is ${result}, you are in a state of obesity, control your diet by consuming less fat.</p>
+    </div>`;
+    index.style.backgroundColor = '#F92205';
   } else if (result >= 35) {
-    index.innerHTML = `<p>Your body mass index is ${result}, you are extremely obese.</p>`;
+    index.innerHTML = `<div class="show-result">
+    <p>Your body mass index is ${result}, you are in a state of extreme obesity, we advise you to visit your local GP 
+    for a diet counseling.</p>
+    </div>`;
+    index.style.backgroundColor = '#FB0BF7';
   }
 }
